2c0895190096a71b03f4d11b325ec1de398c6431
henrygagnier <prosock@protonmail.com>
2023-04-18 21:35:45 -0400

    Add mobile compatiblity to navbar.

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,34 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import "./Navbar.css";
-const logo = "";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <a href="/">
-          <img src={logo} alt="Logo" />
-          
-        </a>
-      </div>
-      <ul className="navbar-menu">
-        <li>
-          <a href="/">Our Mission</a>
-        </li>
-        <li>
-          <a href="/apps">Affiliated Apps</a>
-        </li>
-        <li>
-          <a href="/blog">Blog</a>
-        </li>
-        <li>
-          <a href="/donations">Recent Donations</a>
-        </li>
-      </ul>
-      <div className="navbar-right">
-        <a href="/app/dao" className="navbar-button">
-          Enter Governance
-        </a>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          BlockCharity
+        </Link>
+        <div className="menu-text" onClick={() => setMenuOpen(!menuOpen)}>
+          Menu
+        </div>
+        <ul className={menuOpen ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              to="/l"
+              className="nav-links"
+              onClick={() => setMenuOpen(false)}
+            >
+              Our Mission
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className="nav-links"
+              onClick={() => setMenuOpen(false)}
+            >
+              Affiliated Apps
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/projects"
+              className="nav-links"
+              onClick={() => setMenuOpen(false)}
+            >
+              Blog
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className="nav-links"
+              onClick={() => setMenuOpen(false)}
+            >
+              Recent Donations
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
