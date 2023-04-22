553cc72e69fc429098faf289351092838ef6b26c
henrygagnier <prosock@protonmail.com>
2023-04-22 16:50:33 -0400

    Add logo to navbar

--- src/Main/Navbar/Navbar.jsx
+++ src/Main/Navbar/Navbar.jsx
@@ -10,7 +10,8 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          BlockCharity
+        <img alt="blockcharity logo" width="37.5px" height="32px" src="assets/logo.png"></img>
+        <p className="logo-text">BlockCharity</p>
         </Link>
         <div className="menu-text" onClick={() => setMenuOpen(!menuOpen)}>
           Menu
